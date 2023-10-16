81bae0aefa9f7202d55ebcf70cb6723ec38392c4
EST5228 <loganathan.palani@estuate.com>
2023-10-16 22:29:17 +0530

    React elements

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
+
+const element = <h1>Hello, world</h1>;
+ReactDOM.render(element,document.getElementById('root'));
+
+/*
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,3 +21,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
